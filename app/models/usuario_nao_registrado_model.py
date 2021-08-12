ba3937571866578a83d6f4d7b3684695f67c10b3
Luciano Feder <lucianoo.feder@gmail.com>
2021-08-12 13:24:26 -0300

    feat: created model still need to create relation with usuario table

--- app/models/usuario_nao_registrado_model.py
+++ app/models/usuario_nao_registrado_model.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql.expression import null
 from sqlalchemy.sql.sqltypes import String
 from app.configs.database import db
 
@@ -9,8 +10,8 @@ class UsuarioNaoRegistradoModel(db.Model):
 
     id = Column(Integer, primary_key=True)
 
-    nome = Column(String(150))
-    telefone = Column(String(50), unique=True)
+    nome = Column(String(150), nullable=False)
+    telefone = Column(String(50), unique=True, nullable=False)
 
     def serializer(self):
         return {
