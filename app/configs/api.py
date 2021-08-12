fb31e4214449056551debf861a7f955e8a06d1eb
Luciano Feder <lucianoo.feder@gmail.com>
2021-08-12 12:48:08 -0300

    feat: created model and made the migrations

--- app/configs/api.py
+++ app/configs/api.py
@@ -4,3 +4,6 @@ from flask_restful import Api
 
 def init_app(app: Flask) -> None:
     api = Api(app)
+
+    from app.models.usuario_model import UsuarioModel
+    from app.models.usuario_nao_registrado_model import UsuarioNaoRegistradoModel
---
+++ app/models/usuario_nao_registrado_model.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql.sqltypes import String
+from app.configs.database import db
+
+
+class UsuarioNaoRegistradoModel(db.Model):
+
+    __tablename__ = "usuario_nao_registrado"
+
+    id = Column(Integer, primary_key=True)
+
+    nome = Column(String(150))
+    telefone = Column(String(50), unique=True)
