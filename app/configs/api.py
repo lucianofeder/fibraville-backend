a6bf4b3ea786049b5e4284dcb7687bdc7292c547
Luciano Feder <lucianoo.feder@gmail.com>
2021-08-12 23:32:50 -0300

    feat: created ContasAPagarModel without relation

--- app/configs/api.py
+++ app/configs/api.py
@@ -17,3 +17,5 @@ def init_app(app: Flask) -> None:
     from app.models.visita_tecnica_model import VisitaTecnicaModel
     from app.models.fornecedor_model import FornecedorModel
     from app.models.fornecedor_endereco_model import FornecedorEnderecoModel
+    from app.models.contas_a_pagar_model import ContasAPagarModel
+    
---
+++ app/models/contas_a_pagar_model.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Column, Integer
+from sqlalchemy.sql.sqltypes import Boolean, Date, DateTime, Float, String
+from app.configs.database import db
+from datetime import datetime
+
+
+
+class ContasAPagarModel(db.Model):
+
+    __tablename__ = "contas_a_pagar"
+
+    id = Column(Integer, primary_key=True)
+
+    valor = Column(Float, nullable=False)
+    data_digitado = Column(DateTime, default=datetime.now())
+    data_emissao = Column(Date, nullable=False)
+    data_a_pagar = Column(Date, nullable=False)
+    nfe = Column(String(150))
+    n_documento = Column(String(150))
+    pago = Column(Boolean, default=False)
+
+    def serializer(self):
+        return {
+            "id": self.id,
+            "valor": self.valor,
+            "data_digitado": self.data_digitado,
+            "data_emissao": self.data_emissao,
+            "data_a_pagar": self.data_a_pagar,
+            "nfe": self.nfe,
+            "n_documento": self.n_documento,
+            "pago": self.pago
+        }
