18d06e0ebd6b8e2bc0dc5757b519a8ebf9bb857c
Luciano Feder <lucianoo.feder@gmail.com>
2021-08-19 23:13:50 -0300

    feat: created views for PlanoResource

--- app/services/helper.py
+++ app/services/helper.py
@@ -4,6 +4,10 @@ class BaseServices():
     def save(self):
         db.session.add(self)
         db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
 def add_commit(data):
     db.session.add(data)
