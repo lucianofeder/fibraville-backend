95a36c5045590bee2cf8ff097838f4889315295e
Luciano Feder <lucianoo.feder@gmail.com>
2021-08-30 23:26:18 -0300

    refact: Fornecedor

--- app/models/fornecedor_model.py
+++ app/models/fornecedor_model.py
@@ -25,8 +25,8 @@ class FornecedorModel(db.Model, BaseServices):
     id = Column(Integer, primary_key=True)
 
     razao_social = Column(String(150), nullable=False)
-    nome_fantasia = Column(String(150), nullable=False)
-    cnpj = Column(String(16), nullable=False)
+    nome_fantasia = Column(String(150))
+    cnpj = Column(String(16), nullable=False, unique=True)
     inscricao_estadual = Column(String(9))
     contato = Column(String(150))
 
